e586ddbde0f0b065767e300f389072698e6f10bb
si tran <lesi20061998@gmail.com>
2020-01-13 21:16:33 +0700

    config jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,14 +4,7 @@ import $ from 'jquery';
 window.$ = window.jQuery = $;
 
 import 'jquery-ui/ui/widgets/datepicker.js';
+import 'slick-carousel/slick/slick.min.js';
 
 
-$(document).ready(function () {
-    $('#popup').on('click', function () {
-      
-        $('ul li').toggle(
-            "slow","linear"
-        );
-    });
-});
- 
+ jq
