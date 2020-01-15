b75a40afa2422c2a6db0f0f0c48a75fa6ea8dc01
si tran <lesi20061998@gmail.com>
2020-01-15 14:49:39 +0700

    config

--- public/js/app.js
+++ public/js/app.js
@@ -29896,7 +29896,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\n\n                                color: #\n                                      ^\n      Invalid CSS after \"...         color:\": expected expression (e.g. 1px, bold), was \"#;\"\n      in /Applications/MAMP/htdocs/build-choi-/resources/sass/index.sass (line 69, column 40)\n    at /Applications/MAMP/htdocs/build-choi-/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Applications/MAMP/htdocs/build-choi-/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Applications/MAMP/htdocs/build-choi-/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Applications/MAMP/htdocs/build-choi-/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/Applications/MAMP/htdocs/build-choi-/node_modules/sass-loader/dist/index.js:89:7)\n    at Object.done [as callback] (/Applications/MAMP/htdocs/build-choi-/node_modules/neo-async/async.js:8067:18)\n    at options.error (/Applications/MAMP/htdocs/build-choi-/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ }),
 
