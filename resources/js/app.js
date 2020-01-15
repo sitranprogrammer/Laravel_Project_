99386b60aaec60d551eb7d683e189864774cdb91
si tran <lesi20061998@gmail.com>
2020-01-15 14:49:22 +0700

    config img

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,6 @@ require('./bootstrap');
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 
-import 'jquery-ui/ui/widgets/datepicker.js';
-import 'slick-carousel/slick/slick.min.js';
-
+$('').on('click', function () {
+    $('ul>li').toggle();
+});
